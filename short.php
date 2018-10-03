043b7417eb8abaa700d16fe40813ec0a4ae64ea3
Shivam Kumar <shivamkumar30013@gmail.com>
2018-10-03 06:46:54 +0530

    Update short.php

--- short.php
+++ short.php
@@ -13,14 +13,12 @@ if($conn->connect_error){
 else {
     // echo "Connected Successfully";
 }
-
 //***************** SELECT from database
 $sql = "SELECT * from `pinfo` WHERE `aadhar` = '$aadhar'";
 $result = $conn->query($sql);
 if($result->num_rows > 0){
   //code
 }
-
 //**************** Fetch from database
 $sql = "SELECT * from `pinfo`";
 $result = $conn->query($sql);
@@ -34,7 +32,6 @@ if($result->num_rows > 0){
 else{
     echo "0 results";
 }
-
 //*************** Insert into database
 $sql = "INSERT INTO `pinfo` (fname, lname, mnumber) VALUES ('$fname', '$lname', '$mnumber')";
 if($conn->query($sql) === TRUE){
@@ -43,12 +40,10 @@ if($conn->query($sql) === TRUE){
 else{
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 //*************** Email Validation
 if (!filter_var($emailid, FILTER_VALIDATE_EMAIL)){
     $emailidErr = "Invalid email format"; 
 }
-
 // ************* Function for Slashing redundant
 function test_input($data){
 	$data = trim($data);
@@ -56,7 +51,6 @@ function test_input($data){
 	$data = htmlspecialchars($data);
 	return $data;
 }
-
 // ********* Regex 
 function letters_spaces($data){
 	if(!preg_match("/^[a-zA-Z ]+$/", $data)){
@@ -72,15 +66,96 @@ function numbers_decimal($data){
 }
 if(isset($_POST["submit"])){}
 if(strlen($aadhar) < 12){}
-
 $foneName = $_FILES['fone']['name'];
 $imageFileType = strtolower(pathinfo($location.$name, PATHINFO_EXTENSION));
-
 if(move_uploaded_file($tmp_name, $location.$name)){}
-
 
 ?>
 
+ <!-- NavBar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">!!!</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
 
+<!-- Alert -->
+<div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
 
+<!-- Progress -->
+<div class="progress">
+  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
 
+<!-- Dropdowns -->
+<!-- Example split danger button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-danger">Action</button>
+  <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
+
+<!-- Tables -->
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<!-- Random -->
+<div class="row justify-content-around"></div>
+<div class="row justify-content-between offset-md-3"></div>
+
+<!-- Shadow -->
+<div class="shadow-sm p-3 mb-5 bg-white rounded">Small shadow</div>
